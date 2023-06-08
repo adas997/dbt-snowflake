26be6c1b3c6ae1e77e44ad65e50ea4d8a7fe0a2f
arpan.das@mytoys.de <arpan.das@mytoys.de>
2023-06-08 15:23:10 +0000

    developed stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,22 @@
+select 
+--raw orders
+ord.orderid,
+ord.orderdate,
+ord.shipdate,
+ord.shipmode,
+ord.ordercostprice,
+ord.ordersellingprice,
+ord.ordersellingprice - ord.ordercostprice as orderprofit,
+--raw customer
+cust.customername,
+cust.segment,
+cust.country,
+--raw product
+prd.category,
+prd.productname,
+prd.subcategory
+from {{ ref('raw_orders') }} as ord
+left join {{ ref('raw_customer') }} as cust
+on ord.customerid = cust.customerid
+left join {{ ref('raw_product') }} as prd
+on ord.productid = prd.productid
